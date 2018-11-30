0dea030fbc6548684bb33de7859843424ebcb146
Matthias Eiletz <maei@matthiass-mbp.speedport_w_723v_1_45_000>
2018-11-30 17:14:22 +0100

    Fixed not importing DB when running app

--- app.py
+++ app.py
@@ -16,10 +16,6 @@ app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'matthias' # Das sollte nich öffentlich einsehbar sein
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 ### ACHTUNG
 jwt = JWT(app, authenticate, identity) #/auth
 
